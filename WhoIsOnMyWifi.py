c83ad44a1c42570b66b95d0d36ec6fe5a9daa12c
Bazhunga <zhu.s.kevin@gmail.com>
2014-12-07 18:01:34 +0000

    WhoIsOnMyWifi checks ip address status and then uploads to data.sparkfun

--- WhoIsOnMyWifi.py
+++ WhoIsOnMyWifi.py
@@ -6,6 +6,9 @@ __author__ = 'Bazhunga'
 
 import shlex
 import subprocess
+import urllib
+import urllib2
+from keys import *
 
 def main():
     d_knownpeople = {}
@@ -14,8 +17,11 @@ def main():
     d_knownpeople = read_known_people()
     print d_knownpeople
 
-    a_currentusers = find_users(d_knownpeople)
-    print a_currentusers
+    d_currentusers = find_users(d_knownpeople)
+    print d_currentusers
+
+    update_cloud(d_currentusers)
+
 
 # Read in and create a dictionary of people and their ip addresses
 def read_known_people():
@@ -28,7 +34,7 @@ def read_known_people():
     return d_knownpeople
 
 def find_users(d_knownpeople):
-    users_online = []
+    users_online = {}
     for peeps in d_knownpeople:
         pingString = "ping -n 1 " + str(d_knownpeople[peeps])
         print pingString
@@ -37,15 +43,24 @@ def find_users(d_knownpeople):
         pingResult = ping.stdout.read()
         print pingResult.split()
         if 'unreachable.' not in pingResult.split() and 'timed' not in pingResult.split():
-            users_online.append(peeps)
+            users_online[peeps] = 1
+        else:
+            users_online[peeps] = 0
         print
         print
 
     return users_online
 
-#Scanning for all ip addresses would take way too long
-def scan_ip_addresses():
-    pass
+def update_cloud(d_currentusers):
+    isaacStatus = d_currentusers["Isaac_Padden"]
+    kevinStatus = d_currentusers["Kevin_Zhu"]
+    nivarStatus = d_currentusers["Humphrey_Adams"]
+    url = "http://data.sparkfun.com/input/lz994mazZ0tXV13qxQM1?private_key="+str(privKey)+\
+           "&isaac_padden="+str(isaacStatus)+"&kevin="+str(kevinStatus)+"&nivar_olm="+str(nivarStatus)
+    request = urllib2.Request(url)
+    result = urllib2.urlopen(request)
+    messageReturned = result.read()
+    print messageReturned
 
 
 # def find_mac (userMac):
--- main.py
+++ main.py
@@ -13,13 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+
 import webapp2
+from receivetext import *
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('So, who\'s home?')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/receivetext', ReceiveText)
 ], debug=True)
